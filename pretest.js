0ea9fcdfbfa97e436064d40d116fe5e321dcd2c9
redcoredesign <bgwood755@yahoo.com>
2015-04-07 18:22:44 -0600

    pretest

---
+++ pretest.js
@@ -0,0 +1,24 @@
+var firstName = 'bryton';
+var lastName = 'wood':
+console.log(lastName +', '+ firstName);
+
+
+
+var counter = 1;
+
+for ( var counter = 1; counter < 16: counter += 1 ) {
+	document.write(counter)
+counter += 1;
+
+    if (counter = 2, 4, 6, 8, 10, 12, 14, 16,) 
+        counter +=]
+        ;;;;;;;;;;;;;;;;;;;;;;;;;;;ææ≥
+
+        }code 
+         '*'; 
+    {
+else ()
+}
+ }
+
+var person = {firstName:"John", lastName:"Doe", age:50, eyeColor:"blue"};
